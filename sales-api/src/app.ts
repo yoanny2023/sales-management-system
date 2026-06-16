e2fa54047bb1ec942780467ae5f92f0394325f66
yoanny <manuelvaal2021@gmail.com>
2026-06-17 00:16:42 +0100

    fix cors preflight v2

--- sales-api/src/app.ts
+++ sales-api/src/app.ts
@@ -13,8 +13,8 @@ export const app = express();
 
 app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:3000",
-  methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS",],
-  allowedHeaders: ["Content-Type","Authorization",],
+  methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"]
 }));
 
 app.options("*", cors());
