0214d0fb9ea901bcc4aa07bf3cb52f2f698951e6
yoanny <manuelvaal2021@gmail.com>
2026-06-27 14:10:32 +0100

    temporary database console check

--- sales-api/src/config/prisma.ts
+++ sales-api/src/config/prisma.ts
@@ -1,3 +1,8 @@
 import {PrismaClient} from "@prisma/client"
 
 export const prisma = new PrismaClient();
+
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+await prisma.$connect();
+console.log("Prisma connected");
